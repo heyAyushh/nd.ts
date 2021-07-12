3104877a52bfad322bf9aacd3341569a2c7f971e
Ayush <Ayush-15@live.com>
2021-07-12 16:27:40 +0000

    :construction:

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,13 @@ import { exec } from "child_process";
 import { promisify } from "util";
 
 const execp = promisify(exec);
-execp('').then((stats) => {
-  // Do something with `stats`
-  
-}).catch((error) => {
-  // Handle the error.
-});
 
-exec("cat *.js bad_file | wc -l", (err, stdout, stderr) => {
-  if (err) {
-    // node couldn't execute the command
-    return;
+(async () => {
+  try {
+    const { stdout, stderr } = await execp("ls");
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  } catch (err) {
+    console.error(err);
   }
-
-  // the *entire* stdout and stderr (buffered)
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-});
+})();
