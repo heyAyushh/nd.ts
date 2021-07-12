49b21d304679e66abd211f915c7b0066c0ebc630
Ayush <Ayush-15@live.com>
2021-07-12 22:20:36 +0000

    .

---
+++ README.md
@@ -0,0 +1,2 @@
+# n.ts
+npx n.ts: generate bare minimum node typescript setup asap
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+
+const execp = promisify(exec);
+
+(async () => {
+  try {
+    const { stdout, stderr } = await execp(`echo Hello World`);
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  } catch (err) {
+    console.error(err);
+  }
+})();
